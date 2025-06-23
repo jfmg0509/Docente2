5f89603bd43ec5cd6aa14e15824969bf4d635982
Juan Francisco <jfmg0509@gmail.com>
2025-06-23 17:40:50 -0500

    clase carro

---
+++ src/Carro.java
@@ -0,0 +1,23 @@
+class Carro extends Vehiculo {
+    private int valorPeaje = 2;
+
+    public Carro(String placa) {
+        super(placa);
+    }
+
+    @Override
+    public int getValorPeaje() {
+        return valorPeaje;
+    }
+
+    public void setValorPeaje(int valorPeaje) {
+        if (valorPeaje < 0)
+            throw new IllegalArgumentException("El valor del peaje no puede ser negativo");
+        this.valorPeaje = valorPeaje;
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Carro - Placa: " + placa);
+    }
+}
