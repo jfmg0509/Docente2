e2aacfd2891008e953995dea92db285b9599d03b
Juan Francisco <jfmg0509@gmail.com>
2025-06-23 18:13:45 -0500

    clase moto

---
+++ src/Moto.java
@@ -0,0 +1,24 @@
+// CLASE MOTO
+class Moto extends Vehiculo {
+    private int valorPeaje = 1;
+
+    public Moto(String placa) {
+        super(placa);
+    }
+
+    @Override
+    public int getValorPeaje() {
+        return valorPeaje;
+    }
+
+    public void setValorPeaje(int valor) {
+        if (valor < 0)
+            throw new IllegalArgumentException("El valor del peaje no puede ser negativo.");
+        this.valorPeaje = valor;
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Moto - Placa: " + placa);
+    }
+}
